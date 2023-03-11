c4d44bc1d4514a9dc43b24ca7493a43eb16f3c1d
Doug Summerlin <88894304+dsumm1001@users.noreply.github.com>
2023-03-11 13:21:32 -0500

    v1.0.2
    Fixed error with priority queue where queue was sorted by index instead of c2c. changed closed list to set for speed increase. made maze initialization into function for brevity. re-added commented print statements for debugging. Program runs in correct prioritized strafe now with run time for corner to corner path reduced from 6-7 minutes to 3-4 minutes

--- dijkstra_doug_summerlin.py
+++ dijkstra_doug_summerlin.py
@@ -8,7 +8,7 @@
 # Directory ID: dsummerl
 # ========================================
 # Run as 'python3 Dij_maze_search.py'
-# Press ESC for exit
+# Press CTRL+C for exit
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,7 +16,6 @@ import cv2
 import math
 from queue import PriorityQueue
 import time
-# from collections import deque
 
 def getValidInput(type, maze):
     thresh = 5
@@ -37,65 +36,108 @@ def getValidInput(type, maze):
             break
     return coords
 
+def drawMaze():
+    mazeSize = (250,600)
+
+    # Create blank maze
+    maze = np.zeros((mazeSize[0], mazeSize[1], 3), dtype = np.uint8)
+    maze[:] = (0, 255, 0)
+    cv2.rectangle(maze, pt1=(5,5), pt2=(595,245), color=(255,255,255), thickness= -1)
+
+    # draw rectangle obstacles
+    cv2.rectangle(maze, pt1=(95,0), pt2=(155,105), color=(0,255,0), thickness= -1)
+    cv2.rectangle(maze, pt1=(95,145), pt2=(155,mazeSize[1]), color=(0,255,0), thickness= -1)
+
+    cv2.rectangle(maze, pt1=(100,0), pt2=(150,100), color=(0,0,255), thickness= -1)
+    cv2.rectangle(maze, pt1=(100,150), pt2=(150,mazeSize[1]), color=(0,0,255), thickness= -1)
+
+    # draw hexagonal obstacle and boundary
+    hexBoundPts = np.array([[300, 44], [370, 84],
+                            [370, 166], [300, 206], 
+                            [230, 166], [230,84]])
+    cv2.fillConvexPoly(maze, hexBoundPts, color=(0, 255, 0))
+
+    hexPts = np.array([[300, 125-75], [365, math.ceil(125-37.5)],
+                            [365, math.ceil(125+37.5)], [300, 125+75], 
+                            [235, math.ceil(125+37.5)], [235, math.ceil(125-37.5)]])
+    cv2.fillConvexPoly(maze, hexPts, color=(0, 0, 255))
+
+    # draw triangular obstacle
+    cv2.circle(maze, [460, 25], 5, color=(0, 255, 0), thickness=-1)
+    cv2.circle(maze, [460, 225], 5, color=(0, 255, 0), thickness=-1)
+    cv2.circle(maze, [510, 125], 5, color=(0, 255, 0), thickness=-1)
+
+    cv2.rectangle(maze, pt1=(455,25), pt2=(460,225), color=(0,255,0), thickness= -1)
+
+    triUpperBoundPts = np.array([[460, 25], [465, 22], [516, 125], [510, 125]])
+    cv2.fillConvexPoly(maze, triUpperBoundPts, color=(0, 255, 0))
+
+    triLowerBoundPts = np.array([[510, 125], [516, 125], [465, 228], [460, 225]])
+    cv2.fillConvexPoly(maze, triLowerBoundPts, color=(0, 255, 0))
+
+    triPts = np.array([[460, 25], [460, 225], [510, 125]])
+    cv2.fillConvexPoly(maze, triPts, color=(0, 0, 255))
+    return maze
+
 def checkObstacle(xyCoords, maze):
     if all(maze[xyCoords[1], xyCoords[0]] == [255,255,255]):
         return False
     else:
         return True
 
-# [index, coords, c2c, c, actioncost]    
+# [c2c, index, coords, cost, actioncost]    
 def actMoveRight(curr, maze):
     if (checkObstacle((curr[0]+1,curr[1]), maze) == False):
-        newRight = [None, (curr[0]+1, curr[1]), None, None, 1]
+        newRight = [None, None, (curr[0]+1, curr[1]), None, 1]
         return newRight
     else:
         return None
     
 def actMoveLeft(curr, maze):
     if (checkObstacle((curr[0]-1,curr[1]), maze) == False):
-        newLeft = [None, (curr[0]-1, curr[1]), None, None, 1]
+        newLeft = [None, None, (curr[0]-1, curr[1]), None, 1]
         return newLeft
     else:
         return None
     
 def actMoveUp(curr, maze):
     if (checkObstacle((curr[0],curr[1]+1), maze) == False):
-        newUp = [None, (curr[0], curr[1]+1), None, None, 1]
+        newUp = [None, None, (curr[0], curr[1]+1), None, 1]
         return newUp
     else:
         return None
     
 def actMoveDown(curr, maze):
     if (checkObstacle((curr[0],curr[1]-1), maze) == False):
-        newDown = [None, (curr[0], curr[1]-1), None, None, 1]
+        newDown = [None, None, (curr[0], curr[1]-1), None, 1]
         return newDown
     else:
         return None
     
 def actMoveUpRight(curr, maze):
     if (checkObstacle((curr[0]+1,curr[1]+1), maze) == False):
-        newUpRight = [None, (curr[0]+1, curr[1]+1), None, None, 1.4]
+        newUpRight = [None, None, (curr[0]+1, curr[1]+1), None, 1.4]
         return newUpRight
     else:
         return None
     
 def actMoveUpLeft(curr, maze):
     if (checkObstacle((curr[0]-1,curr[1]+1), maze) == False):
-        newUpLeft = [None, (curr[0]-1, curr[1]+1), None, None, 1.4]
+        newUpLeft = [None, None, (curr[0]-1, curr[1]+1), None, 1.4]
         return newUpLeft
     else:
         return None
     
 def actMoveDownRight(curr, maze):
     if (checkObstacle((curr[0]+1,curr[1]-1), maze) == False):
-        newDownRight = [None, (curr[0]+1, curr[1]-1), None, None, 1.4]
+        newDownRight = [None, None, (curr[0]+1, curr[1]-1), None, 1.4]
         return newDownRight
     else:
         return None
     
 def actMoveDownLeft(curr, maze):
     if (checkObstacle((curr[0]-1,curr[1]-1), maze) == False):
-        newDownLeft = [None, (curr[0]-1, curr[1]-1), None, None, 1.4]
+        newDownLeft = [None, None, (curr[0]-1, curr[1]-1), None, 1.4]
         return newDownLeft
     else:
         return None
@@ -147,48 +189,9 @@ def generatePath(nodeIndex, nodeCoords, maze):
 
 print("\nWelcome to the Dijkstra Maze Finder Program! \n")
 
-mazeSize = (250,600)
-
 outVid = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc(*'MJPG'), 60, (600,250))
 
-# Create blank maze
-maze = np.zeros((mazeSize[0], mazeSize[1], 3), dtype = np.uint8)
-maze[:] = (0, 255, 0)
-cv2.rectangle(maze, pt1=(5,5), pt2=(595,245), color=(255,255,255), thickness= -1)
-
-# draw rectangle obstacles
-cv2.rectangle(maze, pt1=(95,0), pt2=(155,105), color=(0,255,0), thickness= -1)
-cv2.rectangle(maze, pt1=(95,145), pt2=(155,mazeSize[1]), color=(0,255,0), thickness= -1)
-
-cv2.rectangle(maze, pt1=(100,0), pt2=(150,100), color=(0,0,255), thickness= -1)
-cv2.rectangle(maze, pt1=(100,150), pt2=(150,mazeSize[1]), color=(0,0,255), thickness= -1)
-
-# draw hexagonal obstacle and boundary
-hexBoundPts = np.array([[300, 44], [370, 84],
-                         [370, 166], [300, 206], 
-                         [230, 166], [230,84]])
-cv2.fillConvexPoly(maze, hexBoundPts, color=(0, 255, 0))
-
-hexPts = np.array([[300, 125-75], [365, math.ceil(125-37.5)],
-                         [365, math.ceil(125+37.5)], [300, 125+75], 
-                         [235, math.ceil(125+37.5)], [235, math.ceil(125-37.5)]])
-cv2.fillConvexPoly(maze, hexPts, color=(0, 0, 255))
-
-# draw triangular obstacle
-cv2.circle(maze, [460, 25], 5, color=(0, 255, 0), thickness=-1)
-cv2.circle(maze, [460, 225], 5, color=(0, 255, 0), thickness=-1)
-cv2.circle(maze, [510, 125], 5, color=(0, 255, 0), thickness=-1)
-
-cv2.rectangle(maze, pt1=(455,25), pt2=(460,225), color=(0,255,0), thickness= -1)
-
-triUpperBoundPts = np.array([[460, 25], [465, 22], [516, 125], [510, 125]])
-cv2.fillConvexPoly(maze, triUpperBoundPts, color=(0, 255, 0))
-
-triLowerBoundPts = np.array([[510, 125], [516, 125], [465, 228], [460, 225]])
-cv2.fillConvexPoly(maze, triLowerBoundPts, color=(0, 255, 0))
-
-triPts = np.array([[460, 25], [460, 225], [510, 125]])
-cv2.fillConvexPoly(maze, triPts, color=(0, 0, 255))
+maze = drawMaze()
 
 # get start and goal nodes
 start = getValidInput("start", maze)
@@ -199,58 +202,63 @@ startTime = time.time()
 solved = False
 openList = PriorityQueue()
 
-parentDict = {1: None}
-coordDict = {1: start}
-closedDict = {}
-openDict = {1: start}
+# intialize data containers for backtracking
+parentDict = {1:None}
+coordDict = {1:start}
+closedSet = set()
 
-# [index, coords, c2c, c, actioncost]
-startNode = [1, start, 0, 0, 0]
-index = startNode[0]
+# [c2c, index, coords, cost, actioncost]
+startNode = [0, 1, start, 0, 0]
+index = startNode[1]
 
 openList.put(startNode)
+maze[startNode[2][1], startNode[2][0]] = (255,0,255)
 
 while not openList.empty() and solved == False:
     first = openList.get()
-    del openDict[first[0]]
-    closedDict[first[0]] = first[1]
+    closedSet.add(first[2])
 
-    if ((first[1] == goal)):
+    if ((first[2] == goal)):
         elapsedTime = time.time() - startTime
         print ("Yay! Goal node located... Operation took ", elapsedTime, " seconds.")
-        print("Current node index: ", first[0], " and cost: ", first[3])
+        print("Current node index: ", first[1], " and cost: ", round(first[3],2))
         solved = True
 
-        index, coords = generatePath(first[0], first[1], maze)
+        index, coords = generatePath(first[1], first[2], maze)
         # pathNodes.reverse()
         # pathStates.reverse()
         break
 
-    results = searchNode(first[1], maze)
+    results = searchNode(first[2], maze)
+    # print(*results,sep='\n')
  
     for i in results:
-        if not i[1] in closedDict.values():
-            maze[i[1][1], i[1][0]] = (255,0,0)
-            if not i[1] in openDict.values() or i[2] is None:
+        if not i[2] in closedSet:
+            maze[i[2][1], i[2][0]] = (255,0,0)
+            if not any(i[2] in k for k in openList.queue):
                 index += 1
-                i[0] = index
+                i[1] = index
+                i[0] = first[0] + i[4]
+                i[3] = i[0]
 
-                i[2] = round(first[2] + i[4], 2)
-                i[3] = i[2]
-
-                parentDict[i[0]] = first[0]
-                coordDict[i[0]] = i[1]
+                parentDict[i[1]] = first[1]
+                coordDict[i[1]] = i[2]
 
                 openList.put(i)
-                openDict[i[0]] = i[1]
         else:
-            if i[3] is not None:
-                if i[3] > first[2] + i[4]:
-                    parentDict[i[0]] = first[0]
-                    i[2] = round(first[2] + i[4], 2)
-                    i[3] = i[2]
+            if i[3] > first[0] + i[4]:
+                parentDict[i[1]] = first[1]
+                i[0] = first[0] + i[4]
+                i[3] = i[0]
 
     outVid.write(cv2.flip(maze,0))
+
+    # print("No. of visited nodes: ", len(closedSet), 
+    #       "No. of identified nodes: ", index,  
+    #       "Length of Stack: ", openList.qsize(), "\n")
+
+    # iter = input("Iterate?")
+    # print()
 
 if solved == False:
     print ("Failure! Goal node not found")
